382a5fa2ab7ec9a3ec52ea4ba093171e796eeb5b
dhruvv <dhruva.venkataraman@gmail.com>
2019-03-23 03:47:16 +0530

    Update dialogflow_webhook.py

--- dialogflow_webhook.py
+++ dialogflow_webhook.py
@@ -1,13 +1,26 @@
 
 
 from flask import Flask, make_response, request, jsonify
+import requests
+import time
+import json
 import sys
 
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    c = requests.get("https://stonehill-hackathon.firebaseio.com/timestamps.json")
+    tempStamps = json.loads(c.json())
+    lowest_stamps = [tempStamps[key][0] for key in tempStamps.keys()]
+    medicineName = list(tempStamps.keys())[lowest_stamps.index(min(lowest_stamps))]
     req = request.get_json(force=True)
     print(req, file=sys.stderr)
-    return make_response(jsonify({'fulfillmentText':'Your next pill is at 7pm'}))
+    timeStamp = time.asctime(time.localtime(min(lowest_stamps)))
+    timeStamp = timeStamp.split()[3]
+    fulString = "Your next pill is "+medicineName+" at "+timeStamp[:-3]
+    fulfillmentDict = {"fulfillmentText": fulString}
+    print(fulfillmentDict, file=sys.stderr)
+    return make_response(jsonify(fulfillmentDict))
 
