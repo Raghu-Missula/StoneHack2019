2a3a66c9a37f5c04780cfe1f43afc1e16dc1c264
dhruvv <dhruva.venkataraman@gmail.com>
2019-03-22 16:36:20 +0530

    dialogflow's webhook file

---
+++ dialogflow_webhook.py
@@ -0,0 +1,18 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, make_response, request, jsonify
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(force=True)
+    print(req, file=sys.stderr)
+    return make_response(jsonify({'fulfillmentText':'Your next pill is at 7pm'}))
+
