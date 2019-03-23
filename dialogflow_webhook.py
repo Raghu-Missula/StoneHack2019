c53c425c9e04008e48998fd50c8876c0c8387658
dhruvv <dhruva.venkataraman@gmail.com>
2019-03-23 09:24:10 +0530

    Added Fallback

--- dialogflow_webhook.py
+++ dialogflow_webhook.py
@@ -13,14 +13,17 @@ app = Flask(__name__)
 def webhook():
     c = requests.get("https://stonehill-hackathon.firebaseio.com/timestamps.json")
     tempStamps = json.loads(c.json())
-    lowest_stamps = [tempStamps[key][0] for key in tempStamps.keys()]
-    medicineName = list(tempStamps.keys())[lowest_stamps.index(min(lowest_stamps))]
-    req = request.get_json(force=True)
-    print(req, file=sys.stderr)
+    if tempStamps == None:
+        timeStamp = 1553349600#2pm today IST
+        medicineName = "ceftum500"
+    else :
+        lowest_stamps = [tempStamps[key][0] for key in tempStamps.keys()]
+        medicineName = list(tempStamps.keys())[lowest_stamps.index(min(lowest_stamps))]
+    #req = request.get_json(force=True)
+    #print(req, file=sys.stderr)
     timeStamp = time.asctime(time.localtime(min(lowest_stamps)))
     timeStamp = timeStamp.split()[3]
     fulString = "Your next pill is "+medicineName+" at "+timeStamp[:-3]
     fulfillmentDict = {"fulfillmentText": fulString}
     print(fulfillmentDict, file=sys.stderr)
     return make_response(jsonify(fulfillmentDict))
-
